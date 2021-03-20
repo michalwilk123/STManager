5439fa9bf01be0f3e765bc30b05ac47d2a5ded0c
michal <michal.wilk0@yahoo.com>
2021-03-20 22:07:33 +0100

    added global dark theme

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,9 +1,11 @@
 from gui.AppView import AppView
 from appContext import context
+from qtmodern.styles import dark as darkStyleSheet
 
 if __name__ == "__main__":
     import sys
-
+    
+    darkStyleSheet(context.app)
     MainWindow = AppView()
     context.app.aboutToQuit.connect(MainWindow.cleanUp)
     MainWindow.show()
